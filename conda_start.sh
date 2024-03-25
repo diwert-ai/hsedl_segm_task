a8e0668ad98854404be8662b4863285c38f9dc68
Zotov Andrey <mail@mail.mail>
2024-03-25 08:05:34 +0000

    up

--- conda_start.sh
+++ conda_start.sh
@@ -5,6 +5,7 @@ conda create -n hse_dl_task python=3.9 --yes
 conda activate hse_dl_task
 pip install -r requirements.txt
 conda install nvidia/label/cuda-12.1.0::cuda-toolkit 
+pip install protobuf==4.25.3
 conda env export > environment.yml
 
 set +x
--- tensorboard_start.sh
+++ tensorboard_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 source /opt/anaconda3/bin/activate hse_dl_task
-tensorboard --logdir=runs
+tensorboard --logdir=runs --samples_per_plugin "images=30"
 set +x
